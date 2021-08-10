82d42dd078bf15ac6dcd49e9b7209b3ac48edc94
Victor Martens <victormartens@yahoo.com.br>
2021-08-09 22:03:06 -0300

    add dag01.py

---
+++ mod09/dag01.py
@@ -0,0 +1,40 @@
+import airflow
+from airflow.settings import task_instance_mutation_hook
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+args = {
+    'owner':'airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='dag_datas_victor',
+    default_args=args,
+    schedule_interval='@daily',
+    dagrun_timeout=timedelta(minutes=60)
+)
+
+t1 = BashOperator(
+    task_id='task_datas_victor',
+    bash_command='date',
+    dag=dag
+)
+
+t2 = BashOperator(
+    task_id='sleep_10s',
+    bash_command='sleep 10',
+    retries=3,
+    dag=dag
+)
+
+t3 = BashOperator(
+    task_id="saida",
+    bash_command='date > /opt/airflow/logs/date_output.txt',
+    retries=3,
+    dag=dag
+)
+
+t1 >> t2 >> t3
